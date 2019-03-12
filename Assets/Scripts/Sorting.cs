03bf37082ffcb6ec65750999be369e673450ba08
DueSette <44945710+DueSette@users.noreply.github.com>
2019-03-12 13:54:23 +0000

    Week 7 Sorting
    Selection and Insertion Sort methods

---
+++ Assets/Scripts/Sorting.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sorting : MonoBehaviour {
+
+    public int[] numbers;
+    public int[] numbs;
+
+    int i, j;   //Iterators
+
+    void Start ()
+    {   
+        SelectionSort();
+        InsertionSort();
+    }
+
+    //Insertion Sort
+    void InsertionSort()
+    {       
+        for(j = 1; j < numbs.Length; j++)
+        {
+            int i = j;
+            while(i > 0 && numbs[i-1] > numbs[i])
+            {
+                int temp1, temp2;
+                temp1 = numbs[i];
+                temp2 = numbs[i - 1];
+
+                numbs[i] = temp2;
+                numbs[i - 1] = temp1;
+                i--;
+            }
+        }
+    }
+
+    //Selection Sort
+    void SelectionSort()
+    {
+        for (j = 0; j < numbers.Length - 1; j++)
+        {
+            int min = j;
+
+            for (i = j + 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[min])
+                {
+                    min = i;
+                }
+            }
+            if (min != j)
+            {
+                int temp1, temp2;
+                temp1 = numbers[j];
+                temp2 = numbers[min];
+
+                numbers[j] = temp2;
+                numbers[min] = temp1;
+            }
+        }
+    }
+}
